5ec3d261a9e64e7a7f94102d5671427fd463323a
Saiful Adnan <91016311+saifuladnan30@users.noreply.github.com>
2024-05-08 03:23:51 +0600

    Delete saifCars directory

--- saifCars/brands/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Brand
-
-class BrandForm(forms.ModelForm):
-    class Meta:
-        model = Brand
-        fields = ['brand_name']
--- saifCars/brands/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('add_brand/', views.add_brand, name="add_brand")
-]
